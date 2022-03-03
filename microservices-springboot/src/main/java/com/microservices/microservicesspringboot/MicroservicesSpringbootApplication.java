22651614574e9ac7c2f4f55b8598ad05adb867b4
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2022-03-03 14:48:56 -0500

    Spring Boot - Microservices: Se ha implementado el servidor Eureka. El proyecto se está ejecutando correctamente.

--- microservices-springboot/src/main/java/com/microservices/microservicesspringboot/MicroservicesSpringbootApplication.java
+++ microservices-springboot/src/main/java/com/microservices/microservicesspringboot/MicroservicesSpringbootApplication.java
@@ -2,12 +2,16 @@ package com.microservices.microservicesspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
 public class MicroservicesSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesSpringbootApplication.class, args);
+        System.out.println("El proyecto está corriendo correctamente");
     }
 
 }
