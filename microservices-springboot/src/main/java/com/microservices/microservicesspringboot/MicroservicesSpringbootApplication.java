060b103a11d2947a420b864658485d14b709c591
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2022-03-03 14:42:31 -0500

    Spring Boot con micro-servicios: Creando el proyecto

---
+++ microservices-springboot/src/main/java/com/microservices/microservicesspringboot/MicroservicesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.microservicesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSpringbootApplication.class, args);
+    }
+
+}
